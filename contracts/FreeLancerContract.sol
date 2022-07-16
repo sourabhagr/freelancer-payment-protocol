ad0f48d5049e6786ebc7e7690a6e1b335eab8c61
Sourabh Agrawal <sourabhagrawal@Sourabhs-MacBook-Air.local>
2022-07-16 12:05:00 +0530

    Add Enums for contract states

--- contracts/FreeLancerContract.sol
+++ contracts/FreeLancerContract.sol
@@ -6,6 +6,8 @@ contract FreelanceContract {
     address payable burnAddr;
     int8 MAX_CONFIRMATION_ATTEMPS_ALLOWED = 5;
 
+    enum ContractStatus {CREATED, ACCEPTED_BY_FREELANCER, ACCEPTED_BY_COMPANY, ACCEPTED_BY_BOTH, ASSETS_DELIVERED, EXPIRED_DUE_TO_NON_DELIVERY, ASSETS_ACCEPTED, ASSETS_REJECTED, EXPIRED_DUE_TO_ASSETS_REJECTION, EXPIRED_DUE_TO_NO_CONFIRMATION}
+
     struct FContract {
         string deliverables;
         address payable companyAddr;
@@ -14,7 +16,7 @@ contract FreelanceContract {
         uint32 freelancerStakedAmt;
         uint deliveryDate;
         uint settlementDate;
-        int8 state; //TODO: Create enum for states
+        ContractStatus state;
         int8 confirmationAttemps;
     }
 
@@ -22,74 +24,82 @@ contract FreelanceContract {
 
     function createFreelancerContract(string memory _deliverables, address payable _companyAddr, address payable _freelancerAddr, uint32 _companyStakedAmt, uint32 _freelancerStakedAmt, uint _deliveryDelay, uint _settlementDelay) public {
         require((msg.sender == _companyAddr) || (msg.sender == _freelancerAddr));
-        fContracts.push(FContract(_deliverables, _companyAddr, _freelancerAddr, _companyStakedAmt, _freelancerStakedAmt, _deliveryDelay, _settlementDelay, 0, 0));
+        fContracts.push(FContract(_deliverables, _companyAddr, _freelancerAddr, _companyStakedAmt, _freelancerStakedAmt, _deliveryDelay, _settlementDelay, ContractStatus.CREATED, 0));
     }
 
     function acceptCompanyPayment(uint contractId) public payable {
         require(msg.sender == fContracts[contractId].companyAddr);
         require(msg.value == fContracts[contractId].companyStakedAmt);
-        require((fContracts[contractId].state == 0) || (fContracts[contractId].state == 2));
+        require((fContracts[contractId].state == ContractStatus.CREATED) || (fContracts[contractId].state == ContractStatus.ACCEPTED_BY_FREELANCER));
 
         escrowAddr.transfer(fContracts[contractId].companyStakedAmt);
 
-        if (fContracts[contractId].state == 0) {
-            fContracts[contractId].state = 1;
+        if (fContracts[contractId].state == ContractStatus.CREATED) {
+            fContracts[contractId].state = ContractStatus.ACCEPTED_BY_COMPANY;
         } else {
-            fContracts[contractId].state = 3;
+            fContracts[contractId].state = ContractStatus.ACCEPTED_BY_BOTH;
         }
     }
 
     function acceptFreelancerPayment(uint contractId) public payable {
         require(msg.sender == fContracts[contractId].freelancerAddr);
         require(msg.value == fContracts[contractId].freelancerStakedAmt);
-        require((fContracts[contractId].state == 0) || (fContracts[contractId].state == 1));
+        require((fContracts[contractId].state == ContractStatus.CREATED) || (fContracts[contractId].state == ContractStatus.ACCEPTED_BY_COMPANY));
 
         escrowAddr.transfer(fContracts[contractId].freelancerStakedAmt);
 
-        if (fContracts[contractId].state == 0) {
-            fContracts[contractId].state = 2;
+        if (fContracts[contractId].state == ContractStatus.CREATED) {
+            fContracts[contractId].state = ContractStatus.ACCEPTED_BY_FREELANCER;
         } else {
-            fContracts[contractId].state = 3;
+            fContracts[contractId].state = ContractStatus.ACCEPTED_BY_BOTH;
         }
     }
 
     function uploadDeliverables(uint contractId) public {
-        require(fContracts[contractId].state == 3);
+        require(fContracts[contractId].state == ContractStatus.ACCEPTED_BY_BOTH);
         //TODO: Store the deliverable in a filecoin storage
         //TODO: Mint an NFT out of it
-        fContracts[contractId].state = 4;
+        fContracts[contractId].state = ContractStatus.ASSETS_DELIVERED;
     }
 
-    //TODO: How will this function be called?
+    //TODO: Currently we are asking the Company to initiate this but explore automation options in future
     function expireContractOverNoDelivery(uint contractId) public {
-        require(fContracts[contractId].state == 3);
-        require(msg.sender == escrowAddr);
+        require(fContracts[contractId].state == ContractStatus.ACCEPTED_BY_BOTH);
+        require(msg.sender == fContracts[contractId].companyAddr);
 
-        //TODO: How are we setting the sender address for this transfer?
-        fContracts[contractId].companyAddr.transfer(fContracts[contractId].companyStakedAmt);
-        fContracts[contractId].companyAddr.transfer(fContracts[contractId].freelancerStakedAmt / 2);
-        burnAddr.transfer(fContracts[contractId].freelancerStakedAmt / 2);
+        //TODO: Do below transaction only if called beyond Delivery Date
+        if (true) {
+            uint32 companyCompensation = fContracts[contractId].freelancerStakedAmt / 2;
+            uint32 stakeToBeBurned = fContracts[contractId].freelancerStakedAmt - companyCompensation;
+            //TODO: How are we setting the sender address for this transfer?
+            fContracts[contractId].companyAddr.transfer(companyCompensation);
+            burnAddr.transfer(stakeToBeBurned);
+            fContracts[contractId].companyAddr.transfer(fContracts[contractId].companyStakedAmt);
+            fContracts[contractId].state = ContractStatus.EXPIRED_DUE_TO_NON_DELIVERY;
+        }
 
-        fContracts[contractId].state = -3;
     }
 
-    //TODO: How will this function be called?
+    //TODO: Currently we are asking the Freelancer to initiate this but explore automation options in future
     function expiryContractOverNoConfirmation(uint contractId) public {
-        require(fContracts[contractId].state == 4);
-        require(msg.sender == escrowAddr);
+        require(fContracts[contractId].state == ContractStatus.ASSETS_DELIVERED);
+        require(msg.sender == fContracts[contractId].freelancerAddr);
 
-        //TODO: How are we setting the sender address for this transfer?
-        fContracts[contractId].freelancerAddr.transfer(fContracts[contractId].companyStakedAmt);
-        fContracts[contractId].freelancerAddr.transfer(fContracts[contractId].freelancerStakedAmt);
+        //TODO: Do below transaction only if called beyond Settlement Date
+        if (true) {
+            //TODO: How are we setting the sender address for this transfer?
+            fContracts[contractId].freelancerAddr.transfer(fContracts[contractId].companyStakedAmt);
+            fContracts[contractId].freelancerAddr.transfer(fContracts[contractId].freelancerStakedAmt);
 
-        //TODO: What to do with the delivered NFT?
+            //TODO: What to do with the delivered NFT?
 
-        fContracts[contractId].state = -4;
+            fContracts[contractId].state = ContractStatus.EXPIRED_DUE_TO_NO_CONFIRMATION;
+        }
     }
 
     function sendConfirmation(uint contractId, bool _isAccepted) public {
         require(msg.sender == fContracts[contractId].companyAddr);
-        require(fContracts[contractId].state == 4);
+        require(fContracts[contractId].state == ContractStatus.ASSETS_DELIVERED);
         require(fContracts[contractId].confirmationAttemps < MAX_CONFIRMATION_ATTEMPS_ALLOWED);
         fContracts[contractId].confirmationAttemps += 1;
 
@@ -98,19 +108,19 @@ contract FreelanceContract {
             fContracts[contractId].freelancerAddr.transfer(fContracts[contractId].companyStakedAmt);
             fContracts[contractId].freelancerAddr.transfer(fContracts[contractId].freelancerStakedAmt);
             //TODO: Transfer NFT to the company
-            fContracts[contractId].state = -5;
+            fContracts[contractId].state = ContractStatus.ASSETS_ACCEPTED;
         } else {
             if (fContracts[contractId].confirmationAttemps == MAX_CONFIRMATION_ATTEMPS_ALLOWED) {
-                fContracts[contractId].state = -6;
+                fContracts[contractId].state = ContractStatus.EXPIRED_DUE_TO_ASSETS_REJECTION;
                 //TODO: How are we setting the sender address for this transfer?
-                burnAddr.transfer(fContracts[contractId].freelancerStakedAmt / 2);
-                burnAddr.transfer(fContracts[contractId].freelancerStakedAmt / 2);
-                //TODO: Decimal handling when divided by 2?
-                fContracts[contractId].companyAddr.transfer(fContracts[contractId].companyStakedAmt - fContracts[contractId].freelancerStakedAmt / 2);
-                fContracts[contractId].freelancerAddr.transfer(fContracts[contractId].freelancerStakedAmt / 2);
+                uint32 freelancerStakeToBeBurnt = fContracts[contractId].freelancerStakedAmt / 2;
+                uint32 companyStakeToBeBurnt = fContracts[contractId].freelancerStakedAmt / 2;
+                burnAddr.transfer(freelancerStakeToBeBurnt + companyStakeToBeBurnt);
+                fContracts[contractId].companyAddr.transfer(fContracts[contractId].companyStakedAmt - companyStakeToBeBurnt);
+                fContracts[contractId].freelancerAddr.transfer(fContracts[contractId].freelancerStakedAmt - freelancerStakeToBeBurnt);
             } else {
                 //TODO: Update Delivery and Settlement Dates
-                fContracts[contractId].state = 6;
+                fContracts[contractId].state = ContractStatus.ASSETS_REJECTED;
             }
         }
     }
