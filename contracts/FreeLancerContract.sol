f89c79a92264d8d8af0c0583e7e317cb182878fd
Sourabh <sourabh.agrawal031994@gmail.com>
2022-07-25 12:14:53 +0530

    payment in usdt instead of native token

--- contracts/FreeLancerContract.sol
+++ contracts/FreeLancerContract.sol
@@ -1,11 +1,20 @@
 pragma solidity >=0.4.25 <0.9.0;
 
+interface IERC20 {
+    function transfer(address _to, uint256 _value) external returns (bool);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool);
+    function approve(address _spender, uint256 _value) external returns (bool);
+}
+
+//TODO: Import contracts like Ownable and add decorators
+
 contract FreelanceContract {
 
     address payable escrowAddr;
     address payable burnAddr;
     int8 MAX_CONFIRMATION_ATTEMPS_ALLOWED = 5;
     int8 CONTRACT_ACCEPTION_DEADLINE_DAYS = 7;
+    IERC20 usdt = IERC20(address(0xdAC17F958D2ee523a2206206994597C13D831ec7));
 
     enum ContractStatus {CREATED, ACCEPTED_BY_FREELANCER, ACCEPTED_BY_COMPANY, ACCEPTED_BY_BOTH, ASSETS_DELIVERED, EXPIRED_DUE_TO_NON_DELIVERY, ASSETS_ACCEPTED, ASSETS_REJECTED, EXPIRED_DUE_TO_ASSETS_REJECTION, EXPIRED_DUE_TO_NO_CONFIRMATION, EXPIRED_DUE_TO_NO_COMPANY_ACCEPTANCE, EXPIRED_DUE_TO_NO_FREELANCER_ACCEPTANCE}
 
@@ -13,8 +22,8 @@ contract FreelanceContract {
         string deliverables;
         address payable companyAddr;
         address payable freelancerAddr;
-        uint32 companyStakedAmt;
-        uint32 freelancerStakedAmt;
+        uint256 companyStakedAmt;
+        uint256 freelancerStakedAmt;
         uint deliveryDate;
         uint settlementDate;
         ContractStatus state;
@@ -25,7 +34,7 @@ contract FreelanceContract {
 
     FContract[] public fContracts;
 
-    event ContractCreated(string _deliverables, address _companyAddr, address _freelancerAddr, uint32 _companyStakedAmt, uint32 _freelancerStakedAmt, uint _deliveryDelay, uint _settlementDelay, address _createdBy, uint _contractId);
+    event ContractCreated(string _deliverables, address _companyAddr, address _freelancerAddr, uint256 _companyStakedAmt, uint256 _freelancerStakedAmt, uint _deliveryDelay, uint _settlementDelay, address _createdBy, uint _contractId);
     event StakePostedByCompany(uint _contractId);
     event StakePostedByFreelancer(uint _contractId);
     event ContractAccepted(uint _contractId);
@@ -34,7 +43,7 @@ contract FreelanceContract {
     event ContractExpiredDueToNoConfirmation(uint _contractId, uint _amountTransferedToCompany, uint _amountTransferedToFreelancer);  //TODO: Should we add expiry date in the event?
 
 
-    function createFreelancerContract(string memory _deliverables, address payable _companyAddr, address payable _freelancerAddr, uint32 _companyStakedAmt, uint32 _freelancerStakedAmt, uint _deliveryDelay, uint _settlementDelay) public {
+    function createFreelancerContract(string memory _deliverables, address payable _companyAddr, address payable _freelancerAddr, uint256 _companyStakedAmt, uint256 _freelancerStakedAmt, uint _deliveryDelay, uint _settlementDelay) public {
         require((msg.sender == _companyAddr) || (msg.sender == _freelancerAddr));
         fContracts.push(FContract(_deliverables, _companyAddr, _freelancerAddr, _companyStakedAmt, _freelancerStakedAmt, _deliveryDelay, _settlementDelay, ContractStatus.CREATED, 0, 0, ""));
         emit ContractCreated(_deliverables, _companyAddr, _freelancerAddr, _companyStakedAmt, _freelancerStakedAmt, _deliveryDelay, _settlementDelay, msg.sender, fContracts.length-1);
@@ -45,7 +54,7 @@ contract FreelanceContract {
         require(msg.value == fContracts[contractId].companyStakedAmt);
         require((fContracts[contractId].state == ContractStatus.CREATED) || (fContracts[contractId].state == ContractStatus.ACCEPTED_BY_FREELANCER));
 
-        escrowAddr.transfer(fContracts[contractId].companyStakedAmt);
+        usdt.transfer(escrowAddr, fContracts[contractId].companyStakedAmt);
 
         if (fContracts[contractId].state == ContractStatus.CREATED) {
             fContracts[contractId].state = ContractStatus.ACCEPTED_BY_COMPANY;
@@ -62,7 +71,7 @@ contract FreelanceContract {
         require(msg.value == fContracts[contractId].freelancerStakedAmt);
         require((fContracts[contractId].state == ContractStatus.CREATED) || (fContracts[contractId].state == ContractStatus.ACCEPTED_BY_COMPANY));
 
-        escrowAddr.transfer(fContracts[contractId].freelancerStakedAmt);
+        usdt.transfer(escrowAddr, fContracts[contractId].freelancerStakedAmt);
 
         if (fContracts[contractId].state == ContractStatus.CREATED) {
             fContracts[contractId].state = ContractStatus.ACCEPTED_BY_FREELANCER;
@@ -79,7 +88,7 @@ contract FreelanceContract {
         require(fContracts[contractId].state == ContractStatus.ACCEPTED_BY_COMPANY);
         //TODO: Do below transaction only if called beyond Accepted Date + Deadline
         if (true) {
-            fContracts[contractId].companyAddr.transfer(fContracts[contractId].companyStakedAmt);
+            usdt.transferFrom(escrowAddr, fContracts[contractId].companyAddr, fContracts[contractId].companyStakedAmt);
             fContracts[contractId].state = ContractStatus.EXPIRED_DUE_TO_NO_FREELANCER_ACCEPTANCE;
         }
     }
@@ -89,7 +98,7 @@ contract FreelanceContract {
         require(fContracts[contractId].state == ContractStatus.ACCEPTED_BY_FREELANCER);
         //TODO: Do below transaction only if called beyond Accepted Date + Deadline
         if (true) {
-            fContracts[contractId].freelancerAddr.transfer(fContracts[contractId].freelancerStakedAmt);
+            usdt.transferFrom(escrowAddr, fContracts[contractId].freelancerAddr, fContracts[contractId].freelancerStakedAmt);
             fContracts[contractId].state = ContractStatus.EXPIRED_DUE_TO_NO_COMPANY_ACCEPTANCE;
         }
     }
@@ -110,8 +119,8 @@ contract FreelanceContract {
 
         //TODO: Do below transaction only if called beyond Delivery Date
         if (true) {
-            uint32 companyCompensation = fContracts[contractId].freelancerStakedAmt / 2;
-            uint32 stakeToBeBurned = fContracts[contractId].freelancerStakedAmt - companyCompensation;
+            uint256 companyCompensation = fContracts[contractId].freelancerStakedAmt / 2;
+            uint256 stakeToBeBurned = fContracts[contractId].freelancerStakedAmt - companyCompensation;
             //TODO: How are we setting the sender address for this transfer?
             fContracts[contractId].companyAddr.transfer(companyCompensation);
             burnAddr.transfer(stakeToBeBurned);
@@ -155,8 +164,8 @@ contract FreelanceContract {
             if (fContracts[contractId].confirmationAttemps == MAX_CONFIRMATION_ATTEMPS_ALLOWED) {
                 fContracts[contractId].state = ContractStatus.EXPIRED_DUE_TO_ASSETS_REJECTION;
                 //TODO: How are we setting the sender address for this transfer?
-                uint32 freelancerStakeToBeBurnt = fContracts[contractId].freelancerStakedAmt / 2;
-                uint32 companyStakeToBeBurnt = fContracts[contractId].freelancerStakedAmt / 2;
+                uint256 freelancerStakeToBeBurnt = fContracts[contractId].freelancerStakedAmt / 2;
+                uint256 companyStakeToBeBurnt = fContracts[contractId].freelancerStakedAmt / 2;
                 burnAddr.transfer(freelancerStakeToBeBurnt + companyStakeToBeBurnt);
                 fContracts[contractId].companyAddr.transfer(fContracts[contractId].companyStakedAmt - companyStakeToBeBurnt);
                 fContracts[contractId].freelancerAddr.transfer(fContracts[contractId].freelancerStakedAmt - freelancerStakeToBeBurnt);
